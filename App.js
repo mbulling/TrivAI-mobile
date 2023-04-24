fa36a8d0eddb6a9362f1873e827fb3a00c032455
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-04-24 18:16:16 -0400

    Homerouting

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./screens/Home";
-import QuizScreen from "./screens/QuizScreen";
-=======
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
-import Explore from './screens/Explore';
->>>>>>> 89269ecce544dd7eb2cb299609e77d9ffef35c3b
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <QuizScreen topic={"biology"} numberQuestions={3} />
+      <Home />
     </View>
   );
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-import Loading from "./Loading";
-import get_topic_mcq from "./external";
-=======
 import React, { useState } from 'react';
 import { StyleSheet, View, TouchableOpacity, ScrollView } from 'react-native';
 import Widget from './Widget';
-import QuestionScreen from './QuestionScreen';
+import QuizScreen from './QuizScreen';
 import Explore from './Explore';
->>>>>>> 89269ecce544dd7eb2cb299609e77d9ffef35c3b
 
 export default function Home() {
   const [currentScreen, setCurrentScreen] = useState('Home');
 
   const handlePressCreateQuiz = () => {
-    setCurrentScreen('QuestionScreen');
+    setCurrentScreen('QuizScreen');
   };
 
   const handlePressExploreQuizzes = () => {
@@ -37,8 +30,8 @@ export default function Home() {
             </ScrollView>
           </View>
         );
-      case 'QuestionScreen':
-        return <QuestionScreen />;
+      case 'QuizScreen':
+        return <QuizScreen />;
       case 'Explore':
         return <Explore />;
       default:
@@ -52,17 +45,12 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
-    justifyContent: "center",
-    alignItems: "center",
-=======
     justifyContent: 'center',
     alignItems: 'center',
     width: '90%',
   },
   widgetRow: {
     width: '100%',
->>>>>>> 89269ecce544dd7eb2cb299609e77d9ffef35c3b
   },
   scroll: {
     width: '100%',
