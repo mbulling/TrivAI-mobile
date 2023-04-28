a121692b6041c363cb98470a206423e6ddde0d75
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-04-28 00:39:37 -0400

    Refactor CreateQuiz.js

--- screens/CreateQuiz.js
+++ screens/CreateQuiz.js
@@ -7,6 +7,7 @@ import {
   Slider,
   Button,
   TouchableOpacity,
+  SafeAreaView,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import Loading from "./Loading";
@@ -14,6 +15,7 @@ import QuizScreen from "./QuizScreen";
 import { get_topics } from "../lib/external";
 import { useNavigation } from "@react-navigation/native";
 import NavigationContainer from "@react-navigation/native";
+import HorizontalPicker from './HorizontalPicker';
 
 export default function CreateQuiz() {
   const [topic, setTopic] = useState("");
@@ -40,37 +42,36 @@ export default function CreateQuiz() {
     setDifficulty(difficulty);
   };
 
+  const numbers = [1,2,3,4,5,6,7,8,9,10]
+  const difficulties = ['easy','medium','hard']
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Create a Quiz</Text>
+      <View style={styles.topRow}>
+        <View style={styles.pickerWrapper1}>
+          <View style={styles.difficultyBox}>
+            <Text style = {{marginTop: 20, fontSize: 15, fontWeight: 500}}>choose difficulty</Text>
+            <HorizontalPicker values={difficulties} width = {150} itemWidth={200} onValueChange={handleDifficultyChange}/>
+          </View>
+        </View>
+        <View style={styles.pickerWrapper2}>
+          <View style={styles.questionPicker}>
+            <Text style = {{marginTop: 20, fontSize: 15, fontWeight: 500}}>number of questions</Text>
+            <HorizontalPicker values={numbers} width = {200} itemWidth={200} onValueChange={handleNumberChange}/>
+          </View>
+        </View>
+      </View>
+      <View style={styles.outerCircle}>
+        <TouchableOpacity style={styles.button} onPress={handlePressTakeQuiz}>
+          <Text style={styles.buttonText}>Start Quiz</Text>
+        </TouchableOpacity>
+      </View>
       <Text style = {styles.topic}>Enter topic:</Text>
       <TextInput
         style={styles.textInput}
         onChangeText={handleTextChange}
         value={topic}
       />
-      <Text style = {styles.questions}>Select number of questions:</Text>
-      <Slider
-        style={{ width: "80%", marginTop: 10 }}
-        minimumValue={0}
-        maximumValue={10}
-        step={1}
-        value={num_questions}
-        onValueChange={handleNumberChange}
-      />
-      <Text style = {styles.difficulty}>Select difficulty:</Text>
-      <Picker
-        style={{ width: "80%" }}
-        selectedValue={difficulty}
-        onValueChange={handleDifficultyChange}
-      >
-        <Picker.Item label="Easy" value="easy" />
-        <Picker.Item label="Medium" value="medium" />
-        <Picker.Item label="Hard" value="hard" />
-      </Picker>
-      <TouchableOpacity style={styles.button} onPress={handlePressTakeQuiz}>
-        <Text style={styles.buttonText}>Start Quiz</Text>
-      </TouchableOpacity>
     </View>
   );
 }
@@ -78,7 +79,6 @@ export default function CreateQuiz() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     justifyContent: 'flex-start',
     alignItems: 'center',
     width: '100%',
@@ -87,32 +87,92 @@ const styles = StyleSheet.create({
     width: '80%',
     borderRadius: 10,
   },
-  title: {
-    marginTop: 20,
-    fontSize: 36,
+  topRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 200,
+  },
+  pickerWrapper1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 8,
+    marginVertical: 40,
+  },
+  pickerWrapper2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 8,
+    marginRight: 30,
+    marginVertical: 40,
+  },
+  difficultyBox: {
+    alignItems: 'center',
+    backgroundColor: '#fac8f9',
+    borderRadius: 10,
+  },
+  questionPicker: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#83d4fc',
+    borderRadius: 10,
   },
   topic: {
     marginTop: 40,
     marginBottom: 10,
     fontSize: 20,
   },
-  questions: {
-    marginTop: 40,
-    marginBottom: 10,
-  },
-  difficulty: {
-    marginTop: 40,
+  outerCircle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 200,
+    borderWidth: 8,
+    borderColor: '#29578a',
+    width: 330,
+    height: 330,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
   },
   button: {
-    backgroundColor: '#007AFF',
-    paddingHorizontal: 120,
-    paddingVertical: 20,
-    borderRadius: 5,
-    marginTop: 20,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 200,
+    width: 300,
+    height: 300,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+    borderWidth: 5,
+    borderColor: '#29578a',
+    borderStyle: 'solid',
+
   },
   buttonText: {
-    color: '#fff',
-    fontSize: 16,
+    color: '#000',
+    fontSize: 18,
     fontWeight: 'bold',
+  },
+  textInput: {
+    width: '80%',
+    height: 40,
+    borderColor: '#7a42f4',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    backgroundColor: '#FFFFFF',
   },
 });
---
+++ screens/HorizontalPicker.js
@@ -0,0 +1,95 @@
+import React, {useState, useRef, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Animated,
+  Dimensions,
+} from 'react-native';
+
+const HorizontalNumberPicker = ({values, width, itemWidth, onValueChange}) => {
+  const [selected, setSelected] = useState(1);
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  const middleIndex = Math.floor(values.length / 2);
+  const ITEM_SIZE = itemWidth;
+  const ITEM_SPACING = (width - ITEM_SIZE) / 2;
+  const scrollViewRef = useRef(null);
+
+  useEffect(() => {
+    scrollViewRef.current.scrollTo({
+      x: middleIndex * ITEM_SIZE,
+      animated: false,
+    });
+  }, []);
+
+  const onScroll = (event) => {
+    const scrollPosition = event.nativeEvent.contentOffset.x;
+    const selectedIndex = Math.round(scrollPosition / ITEM_SIZE);
+    const newValue = values[selectedIndex];
+
+    if (selected !== newValue) {
+      setSelected(newValue);
+      if (onValueChange) {
+        onValueChange(newValue);
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Animated.ScrollView
+        ref={scrollViewRef}
+        horizontal
+        snapToInterval={ITEM_SIZE}
+        decelerationRate="fast"
+        showsHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {x: scrollX}}}],
+          {useNativeDriver: true, listener: onScroll},
+        )}
+        scrollEventThrottle={16}
+        contentContainerStyle={{paddingHorizontal: ITEM_SPACING}}
+      >
+        {values.map((value, index) => {
+          const inputRange = [(index - 1) * ITEM_SIZE, index * ITEM_SIZE, (index + 1) * ITEM_SIZE];
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0.4, 1, 0.4],
+            extrapolate: 'clamp',
+          });
+          const scale = scrollX.interpolate({
+            inputRange,
+            outputRange: [0.4, 1, 0.4],
+            extrapolate: 'clamp',
+          });
+          return (
+            <View key={index} style={[styles.numberContainer, {width: ITEM_SIZE}]}>
+              <Animated.Text style={[styles.numberText, {opacity, transform: [{scale}]}]}>{value}</Animated.Text>
+            </View>
+          );
+        })}
+      </Animated.ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numberContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numberText: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    marginRight: 30,
+  },
+});
+
+export default HorizontalNumberPicker;
