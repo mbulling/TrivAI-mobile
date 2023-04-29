f24e3d10adf5566d81af440daf80241b964760eb
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-04-29 12:24:45 -0400

    Explore fix format add white shadow

--- screens/Explore.js
+++ screens/Explore.js
@@ -33,9 +33,9 @@ export default function Explore({ navigation }) {
             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
               <View style={styles.subtopicsColumn}>
                 {topic.subtopics.map((subtopic) => (
-                  <TouchableOpacity onPress={() => handlePressTopic(subtopic)}>
+                  <TouchableOpacity onPress={() => handlePressTopic(subtopic)} style={styles.subcontainer}>
                     <View style={styles.subtopic}>
-                      <Text style = {styles.text} key={subtopic}>{subtopic}</Text>
+                      <Text style={styles.text} key={subtopic}>{subtopic}</Text>
                     </View>
                   </TouchableOpacity>
                 ))}
@@ -54,38 +54,45 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    width: '100%',
+  },
+  subcontainer: {
+    padding: 10,
+    paddingLeft: 15,
   },
   topicHeader: {
-    paddingTop: 5,
-    fontSize: 30,
-    marginLeft: 15,
+    paddingTop: 15,
+    fontSize: 26,
     color: "#FFFFFF",
-    font: 'Courier Prime',  
+    font: 'Courier Prime',
     fontWeight: "bold",
+    paddingLeft: 15,
   },
   topicRow: {
-    padding: 10,
     backgroundColor: "#4051A6",
+    paddingTop: 10,
   },
   subtopicsColumn: {
     flexDirection: 'row',
     alignItems: 'flex-start',
   },
   subtopic: {
-    backgroundColor: '#BCD5D4',
+    backgroundColor: '#8c97ca',
     borderRadius: 15,
     justifyContent: 'center',
     alignItems: 'center',
     width: 200,
     height: 100,
-    margin: 10,
-    shadowColor: '#3d3d3d',
-    shadowOffset: { width: -1, height: 3 },
-    shadowOpacity: 0.4,
-    shadowRadius: 2
+    shadowColor: '#ffffff',
+    shadowOffset: { width: -4, height: 4 },
+    shadowOpacity: 0.9,
+    shadowRadius: 0
   },
   text: {
     fontWeight: "bold",
-    fontSize: 15,
+    fontSize: 18,
+    color: "#FFFFFF",
+    textAlign: "center",
+    margin: 10,
   }
 });
