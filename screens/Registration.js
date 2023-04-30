e361863e1b8e015b43ec35a0d84e32faa73ec05d
Mason Bulling <42184555+mbulling@users.noreply.github.com>
2023-04-30 12:55:16 -0400

    style registration page

--- screens/Registration.js
+++ screens/Registration.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import UserContext from "../contexts/user";
-import { View, Button, Text, TextInput, Animated } from "react-native";
+import { View, Button, Text, TextInput, Animated, StyleSheet, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as BE from "../lib/external";
 
@@ -14,6 +14,7 @@ const Registration = () => {
       <Stack.Screen
         name="Register"
         children={(props) => <Register setUser={setUser} {...props} />}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
@@ -47,17 +48,77 @@ const Register = ({ setUser }) => {
   };
 
   return (
-    <View>
-      <Text>Registration Page</Text>
-      <TextInput
-        onChangeText={onChangeName}
-        value={name}
-        placeholder={"Enter your name."}
-        keyboardType={"default"}
-      />
-      <Button onPress={_registrationHandler} title="register" />
+    <View style={styles.wrapper}>
+      <View style={styles.welcome}>
+        <Text style={styles.welcomeText}>Welcome to TrivAI</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.name}>What is your name?</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={onChangeName}
+          value={name}
+          placeholder={"Enter your name."}
+          keyboardType={"default"}
+        />
+        <TouchableOpacity onPress={_registrationHandler} style={styles.button}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 50,
+  },
+  wrapper: {
+    flex: 1,
+  },
+  welcome: {
+    backgroundColor: "#EE5F88",
+    width: "100%",
+    padding: 20,
+    paddingTop: 100,
+  },
+  welcomeText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 36,
+    textAlign: "center",
+    overflow: "wrap",
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+    overflow: "wrap",
+    color: "#4051A6",
+  },
+  textInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginTop: 20,
+    marginBottom: 20,
+    borderRadius: 10,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#4051A6",
+    borderRadius: 10,
+    padding: 10,
+    marginRight: 0,
+    marginLeft: "auto",
+    width: 60,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+  },
+});
 
 export default Registration;
